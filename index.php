79d95f01554c2bdd8c0eb32e9316371e00b470c0
Rajnish <rajnish_pawar90@yahoo.com>
2014-12-09 04:46:44 +0530

    left panel added to index page

---
+++ index.php
@@ -0,0 +1,314 @@
+  <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <title>CIDCO</title>
+        
+        <link rel="stylesheet" href="css/bootstrap.css">
+        
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/light-theme.css" rel="stylesheet">
+        <link href="css/demo.css" rel="stylesheet">
+        <script src="js/bootstrap.js"></script>
+        <script src="js/jquery-ui.js"></script>
+       
+        <script src="js/jquery.js"></script>
+        <script src="js/theme.js"></script>
+        <style>
+            .Headername
+            {
+            font-family:Arial;
+            font-size:14px;
+            color:Black;	
+            font-weight:bold;
+            }
+            .HeaderMarathi
+            {
+            font-family:Mangal;
+            font-size:22px;
+            color:Black;	
+            font-weight:normal;
+            }
+            .SubHeaderName
+            {
+                    font-family:Arial;
+                    font-size:11px;
+                    color:Black;	
+                    font-weight:bold;
+                    line-height:18px;
+            }
+            .SubHeaderNameMarathi
+            {
+                    font-family:Mangal;
+                    font-size:11px;
+                    color:Black;	
+                    font-weight:bold;
+                    line-height:20px;
+            }
+            .lang {
+                    FONT-FAMILY: Arial; COLOR: black; FONT-SIZE: 12px
+            }
+            .lang A {
+                    FONT-FAMILY: Arial; COLOR: black; FONT-SIZE: 12px; TEXT-DECORATION: none
+            }
+            .lang A:hover {
+                    FONT-FAMILY: Arial; COLOR: #3e53a2; FONT-SIZE: 12px; TEXT-DECORATION: none
+            }
+        </style>
+    </head>
+    <body>
+        
+        <div class="jumbotron">
+                <table align="center" width="900px" cellpadding="0" cellspacing="0">
+                    <tr>
+                    <td style="width: 5px">
+                    </td>
+                        <td style="width: 175px">
+                            <a href="#">
+                                <img src="logoenglish.png" alt="" style="border:0" />
+                            </a>
+                        </td>
+                        
+                        <td style="width: 603px" class="Headername" align="center">
+                            <div style="padding-top:28px;">
+                                CITY AND INDUSTRIAL DEVELOPMENT CORPORATION OF MAHARASHTRA LIMITED
+                            </div>
+                            <div style="text-align: center" class="SubHeaderName">
+                                (Government of Maharashtra Undertaking)</div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        <nav id="main-nav">
+        <div class="navigation">
+          <ul class="nav nav-stacked">
+            <li class="active">
+              <a href="#">
+                <i class="icon-dashboard"></i>
+                <span>Dashboard</span>
+              </a>
+            </li>
+            <li class="">
+                      <a class="dropdown-collapse" href="#"><i class="icon-edit"></i>
+              <span>Forms</span>
+              <i class="icon-angle-down angle-down"></i>
+              </a>
+      
+              <ul class="nav nav-stacked">
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/form_styles.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Form styles and features</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/form_components.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Form components</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/validations.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Validations</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/wizard.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Wizard</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a class="dropdown-collapse " href="#">
+                <i class="icon-tint"></i>
+                <span>UI Elements &amp; Widgets</span>
+                <i class="icon-angle-down angle-down"></i>
+              </a>
+              <ul class="nav nav-stacked">
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/ui_elements.html">
+                    <i class="icon-caret-right"></i>
+                    <span>UI Elements</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/widgets.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Widgets</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="">
+              <a href="http://www.bublinastudio.com/flattybs3/buttons_and_icons.html">
+                <i class="icon-star"></i>
+                <span>Buttons &amp; Icons</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-collapse " href="#">
+                <i class="icon-cogs"></i>
+                <span>Components</span>
+                <i class="icon-angle-down angle-down"></i>
+              </a>
+              <ul class="nav nav-stacked">
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/charts.html">
+                    <i class="icon-bar-chart"></i>
+                    <span>Charts</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/address_book.html">
+                    <i class="icon-envelope"></i>
+                    <span>Address book</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/chats.html">
+                    <i class="icon-comments"></i>
+                    <span>Chats</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/filetrees.html">
+                    <i class="icon-list-ul"></i>
+                    <span>File trees</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/fileupload.html">
+                    <i class="icon-file"></i>
+                    <span>Fileupload</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/todo.html">
+                    <i class="icon-list-alt"></i>
+                    <span>Todo list</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/wysiwyg.html">
+                    <i class="icon-paste"></i>
+                    <span>WYSIWYG</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="">
+              <a href="http://www.bublinastudio.com/flattybs3/tables.html">
+                <i class="icon-table"></i>
+                <span>Tables</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="http://www.bublinastudio.com/flattybs3/grid.html">
+                <i class="icon-th"></i>
+                <span>Grid</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="http://www.bublinastudio.com/flattybs3/type.html">
+                <i class="icon-font"></i>
+                <span>Typography</span>
+              </a>
+            </li>
+            <li class="">
+              <a href="http://www.bublinastudio.com/flattybs3/calendar.html">
+                <i class="icon-calendar"></i>
+                <span>Calendar</span>
+              </a>
+            </li>
+           
+            <li>
+              <a class="dropdown-collapse " href="#">
+                <i class="icon-cog"></i>
+                <span>Layouts</span>
+                <i class="icon-angle-down angle-down"></i>
+              </a>
+              <ul class="nav nav-stacked">
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/closed_navigation.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Closed navigation</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/fixed_header.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Fixed header</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/fixed_navigation.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Fixed navigation</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="http://www.bublinastudio.com/flattybs3/fixed_navigation_and_header.html">
+                    <i class="icon-caret-right"></i>
+                    <span>Fixed navigation &amp; header</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="">
+              <a href="http://www.bublinastudio.com/flattybs3/email_templates.html">
+                <i class="icon-mail-reply"></i>
+                <span>Email templates</span>
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-collapse" href="#">
+                <i class="icon-folder-open-alt"></i>
+                <span>Four level dropdown</span>
+                <i class="icon-angle-down angle-down"></i>
+              </a>
+              <ul class="nav nav-stacked">
+                <li>
+                  <a class="dropdown-collapse" href="#">
+                    <i class="icon-caret-right"></i>
+                    <span>Second level</span>
+                    <i class="icon-angle-down angle-down"></i>
+                  </a>
+                  <ul class="nav nav-stacked">
+                    <li>
+                      <a class="dropdown-collapse" href="#">
+                        <i class="icon-caret-right"></i>
+                        <span>Third level</span>
+                        <i class="icon-angle-down angle-down"></i>
+                      </a>
+                      <ul class="nav nav-stacked">
+                        <li>
+                          <a href="#">
+                            <i class="icon-caret-right"></i>
+                            <span>Fourth level</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <i class="icon-caret-right"></i>
+                            <span>Another fourth level</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </nav>
+        <div class="row">
+        </div>
+    </body>
+</html>
+    
--- login.php
+++ login.php
@@ -7,9 +7,6 @@
         <title>CIDCO</title>
         
         <link rel="stylesheet" href="css/bootstrap.css">
-        <link rel="stylesheet" href="css/bootswatch.css">
-        <link href="css/bootstrap-responsive.css" rel="stylesheet">
-        <link href="css/custom.css" rel="stylesheet">
         <link href="css/font-awesome.css" rel="stylesheet">
         <style>
             .Headername
